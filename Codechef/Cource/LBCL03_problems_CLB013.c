a8bb9fec2234a520328ff8e539c0c81f3bc2e609
SayondeepYT <sayondeepghoshg@gmail.com>
2024-03-18 21:54:03 +0530

    .

---
+++ Codechef/Cource/LBCL03_problems_CLB013.c
@@ -0,0 +1,9 @@
+// Task
+// Write a program to output 7 and 3 on a single line, space separated, using a single printf statement.
+
+#include <stdio.h>
+
+int main(void) {
+    printf("%d %d\n", 7, 3);
+    return 0;
+}
